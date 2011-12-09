712216358e4fd0521044ca5c4cbbc9a5a69e2a41
Jonathan Christian <jchristian>
2011-12-09 11:33:18 -0800

    Pushing new changes

--- source/app/infrastructure/containers/basic/DependencyContainer.cs
+++ source/app/infrastructure/containers/basic/DependencyContainer.cs
@@ -2,28 +2,29 @@
 
 namespace app.infrastructure.containers.basic
 {
-  public class DependencyContainer :IFetchDependencies
+  public class DependencyContainer : IFetchDependencies
   {
     IFindDependencyFactories dependency_finder;
 
-      public DependencyContainer(IFindDependencyFactories dependency_finder)
+    public DependencyContainer(IFindDependencyFactories dependency_finder)
+    {
+      this.dependency_finder = dependency_finder;
+    }
+
+    public Dependency a<Dependency>()
+    {
+      Dependency dependency;
+
+      try
       {
-          this.dependency_finder = dependency_finder;
+        dependency = (Dependency)dependency_finder.get_factory_that_can_create(typeof(Dependency)).create();
+      }
+      catch (Exception e)
+      {
+        throw new DependencyCreationException(e, typeof(Dependency));
       }
 
-      public Dependency a<Dependency>()
-      {
-        Dependency dependency;
-
-        try
-        {
-          dependency = (Dependency)dependency_finder.get_factory_that_can_create(typeof(Dependency)).create();
-        }
-        catch (Exception e)
-        {
-          throw new DependencyCreationException(e, typeof(Dependency));
-        }
-        return dependency;
-      }
+      return dependency;
+    }
   }
 }
