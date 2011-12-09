b9626b65086e0e1656993752d60b0c58832ec3a9
Jonathan Christian <jchristian>
2011-12-09 14:05:02 -0800

    Pushing new changes

--- source/app/infrastructure/containers/basic/DependencyFactoryRegistry.cs
+++ source/app/infrastructure/containers/basic/DependencyFactoryRegistry.cs
@@ -7,15 +7,23 @@ namespace app.infrastructure.containers.basic
   public class DependencyFactoryRegistry : IFindDependencyFactories
   {
     IEnumerable<ICreateASingleDependency> factories;
+    MissingDependencyFactoryException exception_factory;
 
-    public DependencyFactoryRegistry(IEnumerable<ICreateASingleDependency> factories)
+    public DependencyFactoryRegistry(IEnumerable<ICreateASingleDependency> factories,
+                                     MissingDependencyFactoryException exception_factory)
     {
       this.factories = factories;
+      this.exception_factory = exception_factory;
     }
 
     public ICreateASingleDependency get_factory_that_can_create(Type dependency)
     {
-      return factories.First(x => x.can_create(dependency));
+      var matching_factory = factories.FirstOrDefault(x => x.can_create(dependency));
+      if (matching_factory == null)
+      {
+        throw exception_factory(dependency);
+      }
+      return matching_factory;
     }
   }
 }
