d1bf9f1c67ddff9491c222f9a9fb02703cdee30e
Jonathan Christian <jchristian>
2011-12-09 11:12:29 -0800

    Pushing new changes

--- source/app/infrastructure/containers/basic/DependencyContainer.cs
+++ source/app/infrastructure/containers/basic/DependencyContainer.cs
@@ -2,9 +2,16 @@
 {
   public class DependencyContainer :IFetchDependencies
   {
+    IFindDependencyFactories dependency_factory_registry;
+
+    public DependencyContainer(IFindDependencyFactories dependency_factory_registry)
+    {
+      this.dependency_factory_registry = dependency_factory_registry;
+    }
+
     public Dependency a<Dependency>()
     {
-      throw new System.NotImplementedException();
+      return (Dependency)dependency_factory_registry.get_factory_that_can_create(typeof(Dependency)).create();
     }
   }
 }
