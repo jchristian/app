20a06194b63d9fc5c76e21d8fd397d0af329cf46
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-09 15:27:42 -0700

    Updated

--- source/app/infrastructure/containers/basic/DependencyFactoryRegistry.cs
+++ source/app/infrastructure/containers/basic/DependencyFactoryRegistry.cs
@@ -18,12 +18,7 @@ namespace app.infrastructure.containers.basic
 
     public ICreateASingleDependency get_factory_that_can_create(Type dependency)
     {
-      var matching_factory = factories.FirstOrDefault(x => x.can_create(dependency));
-      if (matching_factory == null)
-      {
-        throw exception_factory(dependency);
-      }
-      return matching_factory;
+      return factories.first_or_throw(x => x.can_create(dependency), exception_factory(dependency));
     }
   }
 }
